0226e11587ee0fad4317012593208347060fd4a0
TailorSnix <r.schneider89@icloud.com>
2014-06-05 21:20:50 +0300

    added a test class

---
+++ RSTest.h
@@ -0,0 +1,13 @@
+//
+//  RSTest.h
+//  Git Test
+//
+//  Created by René Schneider on 05.06.14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RSTest : NSObject
+
+@end
---
+++ RSTest.m
@@ -0,0 +1,13 @@
+//
+//  RSTest.m
+//  Git Test
+//
+//  Created by René Schneider on 05.06.14.
+//
+//
+
+#import "RSTest.h"
+
+@implementation RSTest
+
+@end
